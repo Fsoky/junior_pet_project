b1c0d4ce00c41f965c431319bcd82f5da27ccedd
Fsoky <cyberuest0x12@gmail.com>
2026-04-21 01:36:50 +0500

    Refactor Config class to enhance database and Redis configuration properties

--- src/core/config.py
+++ src/core/config.py
@@ -16,12 +16,33 @@ class AuthJWT(BaseModel):
 
 
 class Config(BaseSettings):
-    DB_URL: SecretStr
+    POSTGRES_DB: SecretStr
+    POSTGRES_USER: SecretStr
+    POSTGRES_PASSWORD: SecretStr
+    POSTGRES_HOST: SecretStr
+    POSTGRES_PORT: SecretStr
     
-    APP_HOST: str = "localhost"
+    REDIS_HOST: str
+    REDIS_PORT: str
+    REDIS_PASSWORD: SecretStr | None = None
+    
+    APP_HOST: str = "0.0.0.0"
     APP_PORT: int = 8000
     
     AUTH_JWT: AuthJWT = AuthJWT()
+
+    @property
+    def db_url(self) -> str:
+        return (
+            "asyncpg://"
+            f"{self.POSTGRES_USER.get_secret_value()}:{self.POSTGRES_PASSWORD.get_secret_value()}"
+            f"@{self.POSTGRES_HOST.get_secret_value()}:{self.POSTGRES_PORT.get_secret_value()}"
+            f"/{self.POSTGRES_DB.get_secret_value()}"
+        )
+    
+    @property
+    def redis_pwd(self) -> str | None:
+        return self.REDIS_PASSWORD.get_secret_value() if self.REDIS_PASSWORD else None
 
     model_config = SettingsConfigDict(
         env_file=ROOT_DIR / ".env",
@@ -30,10 +51,12 @@ class Config(BaseSettings):
 
 
 config = Config() # type: ignore[call-arg]
-redis = redis_asyncio.Redis()
+redis = redis_asyncio.Redis(
+    host=config.REDIS_HOST, port=config.REDIS_PORT, password=config.redis_pwd
+)
 
 TORTOISE_ORM = {
-    "connections": {"default": config.DB_URL.get_secret_value()},
+    "connections": {"default": config.db_url},
     "apps": {
         "models": {
             "models": ["src.db.models", "aerich.models"],
